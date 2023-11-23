3d8a90c7a3064e58b1cf63d61c35ea68fcab84b1
Victoria <130211365+stVictoriaV@users.noreply.github.com>
2023-11-23 20:42:24 +0200

    Add files via upload

---
+++ HotelDB.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE HotelsDB;
+USE HotelsDB;
+
+CREATE TABLE Cities
+(
+cityID int primary key not null auto_increment,
+cityName varchar(20)
+);
+
+CREATE TABLE HOTELS
+(
+hotelID  int primary key not null auto_increment,
+hotelName varchar(20),
+starsCounts int not null,
+creationDate date,
+cityId int,
+foreign key (cityId) references Cities (cityID)
+);
+
+CREATE TABLE Rooms
+(
+roomID int primary key not null auto_increment,
+comfortLevel varchar(20),
+hotelID int,
+foreign key (hotelID) references Hotels(hotelID)
+);
+
+CREATE TABLE Clients
+(
+clientID int primary key not null auto_increment,
+clientName varchar(50),
+clientNumber long
+);
+
+CREATE TABLE Orders
+(
+orderID int primary key not null auto_increment,
+clientID int,
+roomId int,
+foreign key (clientID) references Clients(clientID),
+foreign key (roomId) references Rooms(roomID)
+);
